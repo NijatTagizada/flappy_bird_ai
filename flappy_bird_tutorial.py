3f9edca065042c08e9d8b4e4789d856fe24655b0
Nijat <tagizada.nicat@gmail.com>
2021-07-06 18:50:50 +0400

    added bird class

--- flappy_bird_tutorial.py
+++ flappy_bird_tutorial.py
@@ -11,3 +11,34 @@ BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bi
              pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
              pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
              ]
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25  # how much tilt
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0  # when we last jump
+        self.vel = 0
+        self.height = y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+
+
